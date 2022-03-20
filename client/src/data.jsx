a848db02b00f1bf793c26ffeaeb5ff98e6710f47
Andi Conner <andiconner@icloud.com>
2022-03-20 18:25:35 -0400

    navigation and slider

---
+++ client/src/data.jsx
@@ -0,0 +1,23 @@
+
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/LrQLkRL/slide-1.png",      title: "A BOOK CAN TAKE YOU PLACES",
+      desc: "GET 30% OFF ON TRAVEL BOOKS.",
+      bg: "fbf0f4",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/Tr1Lp58/slider2.png",
+      title: "THE MORE YOU BUY THE MORE YOU SAVE",
+      desc: "GET 10% OFF FOR ANY ADDITIONAL BOOK (UP TO 40%).",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/HYmTSm2/slider-3.png",
+      title: "BECOME AN ALVANDI MEMBER",
+      desc: "AND ENJOY ALL THE BENEFITS",
+      bg: "d6f3f9",
+    },
+  ];
