a70b386f6a89e26ac3a7f0d8fbac8c358890941e
Andi Conner <andiconner@icloud.com>
2022-03-20 20:09:35 -0400

    categories

--- client/src/data.jsx
+++ client/src/data.jsx
@@ -21,3 +21,21 @@ export const sliderItems = [
       bg: "d6f3f9",
     },
   ];
+
+export const categories = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/cg3L378/category-1.png",
+      title: "DRAMA",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/MDNBvgJ/category-2.png",
+      title: "HISTORY",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/8xSF8xy/category-3.png",
+      title: "ADVENTURE",
+    },
+  ];
