08f6370afd55da4b6118ebc09972e0ec1753742a
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-03-29 19:26:54 -0400

    change / to * on serve.js

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === 'production') {
 // app.use(routes);
 
 //get all
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
